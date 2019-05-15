1eb9acd6731b11f0909b8fe7797681be4198f7ea
viktorfa <vikfand@gmail.com>
2019-05-15 09:08:20 +0200

    Remove some code from listings to make them shorter.

--- listings/InternalMatch.sol
+++ listings/InternalMatch.sol
@@ -1,33 +1,18 @@
-pragma solidity >=0.5.0 <0.6.0;
-
-import {LotteryMaster} from "./LotteryMaster.sol";
-import {AbstractLotteryMatch} from "./AbstractLotteryMatch.sol";
-
-
-/**
- * Match as a digital coin toss between two players. 
- * A tournament is made from a tree of these matches.
- **/
 contract InternalMatch is AbstractLotteryMatch{
+    address public alice;
+    address public bob;
     
-    address public alice;  // Player 1 of the match.
-    address public bob;    // Player 2 of the match.
+    mapping(address => bytes32) public commitments;
+    mapping(address => uint256) public secrets;
     
-    mapping(address => bytes32) public commitments;  // Commitments alice and bob have made.
-    mapping(address => uint256) public secrets;      // Secrets, which are preimages to the commitments, alice and bob have made.
+    AbstractLotteryMatch public left;
+    AbstractLotteryMatch public right;
     
-    AbstractLotteryMatch public left;   // One of the matches for qualifying to this match. A contract address.
-    AbstractLotteryMatch public right;  // One of the matches for qualifying to this match. A contract address.
-    
-    uint256 public tCommit;  // Block height after which making commitments is possible.
-    uint256 public tReveal;  // Block height after which making reveals is possible. And commitments no longer possible.
-    uint256 public tPlay;  // Block height after which deciding the winner is possible. And reveals no longer possible.
-    
+    uint256 public tCommit;
+    uint256 public tReveal;
+    uint256 public tPlay;
 
     constructor(uint256 _tCommit, uint256 _tReveal, uint256 _tPlay, AbstractLotteryMatch _left, AbstractLotteryMatch _right) public {
-        // FOR TESTING require(_tCommit < _tReveal);
-        // FOR TESTING require(_tReveal < _tPlay);
-        
         tCommit = _tCommit;
         tReveal = _tReveal;
         tPlay = _tPlay;
@@ -36,13 +21,9 @@ contract InternalMatch is AbstractLotteryMatch{
         right = _right;
     }
     
-    
-    /**
-     * Have a player commit to a value for the ditial coin toss.
-     */
     function commit(bytes32 _c) public {
-        // FOR TESTING require(tCommit < block.number);
-        // FOR TESTING require(tReveal > block.number);
+        require(tCommit < block.number, "Too early to commit.");
+        require(tReveal > block.number, "Too late to commit.");
         
         alice = left.getWinner();
         bob = right.getWinner();
@@ -50,61 +31,5 @@ contract InternalMatch is AbstractLotteryMatch{
         require(commitments[msg.sender] == 0, "Player has already commited to this match.");
         
         commitments[msg.sender] = _c;
-    }
-    
-    /**
-     * Have a player reveal the value previously commited to for the digital coin toss.
-     */
-    function reveal(uint256 _s) public {
-        // FOR TESTING require(tReveal < block.number);
-        // FOR TESTING require(tPlay > block.number);
-        
-        require(keccak256(abi.encodePacked(msg.sender, _s)) == commitments[msg.sender], "Secret not preimage of commitment.");
-        
-        secrets[msg.sender] = _s;
-    }
-    
-    /**
-     * Implicitly calculate the winner by performing the digital coin toss.
-     */
-    function getWinner() public view returns (address winner) {
-        // FOR TESTING require(tPlay < block.number);
-
-        // NOTE: Be careful of an adversary calling this function
-        // before a winner should be determined.
-
-        // Check if any player is missing
-        if (alice != address(0) && bob == address(0)) {
-            return alice;
-        } else if (alice == address(0) && bob != address(0)) {
-            return bob;
-        } else if (alice == address(0) && bob == address(0)) {
-            return address(0);
-        }
-        
-        // Check if parties have made commitments.
-        if (commitments[alice] != 0 && commitments[bob] == 0) {
-            return alice;
-        } else if (commitments[alice] == 0 && commitments[bob] != 0) {
-            return bob;
-        } else if (commitments[alice] == 0 && commitments[bob] == 0) {
-            return address(0);
-        }
-
-        // Check if parties have revealed.
-        if (secrets[alice] != 0 && secrets[bob] == 0) {
-            return alice;
-        } else if (secrets[alice] == 0 && secrets[bob] != 0) {
-            return bob;
-        } else if (secrets[alice] == 0 && secrets[bob] == 0) {
-            return address(0);
-        }
-        
-        // Both parties have revealed, let's toss the coin.
-        if ((secrets[alice] ^ secrets[bob]) % 2 == 0) {
-            return alice;
-        } else {
-            return bob;
-        }
     }
 }
--- listings/LotteryMaster.sol
+++ listings/LotteryMaster.sol
@@ -1,31 +1,17 @@
-pragma solidity >=0.5.0 <0.6.0;
-
-import {AbstractLotteryMatch} from "./AbstractLotteryMatch.sol";
-
-
-/**
- * The lottery master contract which individual 1v1 matches reference.
- */
 contract LotteryMaster {
-    // TODO Need some way of checking whether enough players have joined by tStart
+    uint256 public N;
+    uint256 public price;
+    uint256 public tStart;
+    uint256 public tFinal;
     
-    address[] public players;  // All players who have made a deposit.
-    mapping(address => uint256) public deposits;  // The value of deposits players have made.
-    
-    address public owner;  // Owner of this contract.
-    
-    uint256 public price;  // Price in wei for buying a ticket.
-    uint256 public N;  // Max number of players in the lottery.
-    
-    uint256 public nPlayers;  // Number of players currently joined.
-    
-    uint256 public tStart;      // Start block height of the lottery.
-    uint256 public tFinal;  // Block height when the lottery is over and withdrawals can be made.
-    
-    AbstractLotteryMatch public finalMatch;  // Reference to the final match which decides the winner.
+    mapping(address => uint256) public deposits;
+    address[] public players;
 
-    bool public isInitialized;  // Whether the lottery is ready to take deposits.
-    bool public isFull;  //  Whether the lottery is full and ready to play.
+    AbstractLotteryMatch public finalMatch;
+    uint256 public nPlayers;
+    bool public isInitialized;
+    bool public isFull;
+    address public owner;
     
     constructor(uint256 _N, uint256 _price, uint256 _tStart, uint256 _tFinal) public {
         require(_tStart < _tFinal, "Time limits invalid. Stop time is before start time.");
@@ -38,30 +24,20 @@ contract LotteryMaster {
         owner = msg.sender;
     }
     
-    /**
-     * Set the final match of the lottery. 
-     * The lottery should not be able to start before this is set. 
-     * It's up to participants to validate that this final match is the correct contract.
-     */
     function setFinalMatch(AbstractLotteryMatch _finalMatch) public {
         require(msg.sender == owner, "Only owner can set final match.");
-        require(finalMatch == AbstractLotteryMatch(0), "Final match is already set.");  // Make sure finalMatch is immutable once set.
+        require(finalMatch == AbstractLotteryMatch(0), "Final match is already set.");
         finalMatch = _finalMatch;
 
         isInitialized = true;
     }
     
-    
-    /**
-     * Players can make a deposit to join the lottery. This is 
-     * equivalent to buying a ticket.
-     */
     function deposit() public payable {
-        // FOR TESTING require(block.number < t0, "Too late to deposit now.");
+        require(block.number < tStart, "Too late to deposit.");
         require(finalMatch != AbstractLotteryMatch(0), "Final match not set. Lottery not initialized yet.");
         require(msg.value == price, "Transaction value is not equal to ticket price.");
         require(isFull == false, "Lottery is full");
-        // FOR TESTING require(deposits[msg.sender] == 0, "Player has already deposited to this lottery.");
+        require(deposits[msg.sender] == 0, "Player has already deposited to this lottery.");
         
         players.push(msg.sender);
         deposits[msg.sender] = msg.value;
@@ -72,19 +48,12 @@ contract LotteryMaster {
         }
     }
     
-    /**
-     * After the predetermined end time of the lottery has passed, then either
-     * (a) the winner can withdraw their prize, or (b) there is no winner and
-     * participants can withdraw their deposit.
-     */
     function withdraw() public {
         address lotteryWinner = finalMatch.getWinner();
         if (lotteryWinner != address(0)) {
-            // The winner can withdraw their prize.
             require(msg.sender == lotteryWinner, "Player is not winner of lottery.");
             msg.sender.transfer(address(this).balance);
         } else {
-            // Lottery has concluded without a winner.
             require(block.number > tFinal, "Lottery stop time is not reached.");
             msg.sender.transfer(deposits[msg.sender]);
         }
@@ -92,12 +61,5 @@ contract LotteryMaster {
     
     function getPlayer(uint256 index) public view returns (address player) {
         player = players[index];
-    }
-
-    /**
-     * The only way to get an array is to make a function to get it.
-     */
-    function getPlayers() public view returns (address[] memory _players) {
-        _players = players;
     }
 }
